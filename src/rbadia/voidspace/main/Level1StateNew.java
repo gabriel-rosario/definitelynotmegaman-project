963d73be07cc6a881a72c871007b8fc087e96ae5
Javi <Javi@10.31.57.201>
2017-12-19 20:44:45 -0400

    Update: New Levels

---
+++ src/rbadia/voidspace/main/Level1StateNew.java
@@ -0,0 +1,14 @@
+package rbadia.voidspace.main;
+
+import rbadia.voidspace.graphics.GraphicsManager;
+import rbadia.voidspace.sounds.SoundManager;
+
+public class Level1StateNew extends Level1State {
+
+	public Level1StateNew(int level, MainFrame frame, GameStatus status, LevelLogic gameLogic,
+			InputHandler inputHandler, GraphicsManager graphicsMan, SoundManager soundMan) {
+		super(level, frame, status, gameLogic, inputHandler, graphicsMan, soundMan);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/rbadia/voidspace/main/MegaManMain.java
+++ src/rbadia/voidspace/main/MegaManMain.java
@@ -54,8 +54,8 @@ public class MegaManMain {
 
 			GameStatus gameStatus = new GameStatus();
 			gameStatus.setLivesLeft(3);
-			LevelState level1State = new Level1State(1, frame, gameStatus, gameLogic, inputHandler, graphicsMan, soundMan);
-			LevelState level2State = new Level2State(2, frame, gameStatus, gameLogic, inputHandler, graphicsMan, soundMan);
+			LevelState level1State = new Level1StateNew(1, frame, gameStatus, gameLogic, inputHandler, graphicsMan, soundMan);
+			LevelState level2State = new Level2StateNew(2, frame, gameStatus, gameLogic, inputHandler, graphicsMan, soundMan);
 			LevelState level3State = new Level3State(3, frame, gameStatus, gameLogic, inputHandler, graphicsMan, soundMan);
 			LevelState level4State = new Level4State(4, frame, gameStatus, gameLogic, inputHandler, graphicsMan, soundMan);
 
