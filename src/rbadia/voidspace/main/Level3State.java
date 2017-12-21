99622fe9f501c9cb1f4795b3e4fd8dfad08245b7
Javi <Javi@10.31.57.201>
2017-12-21 02:40:21 -0400

    All background added

--- src/rbadia/voidspace/main/Level3State.java
+++ src/rbadia/voidspace/main/Level3State.java
@@ -1,6 +1,10 @@
 package rbadia.voidspace.main;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
 
 import rbadia.voidspace.graphics.GraphicsManager;
 import rbadia.voidspace.model.Asteroid;
@@ -9,34 +13,81 @@ import rbadia.voidspace.sounds.SoundManager;
 
 
 public class Level3State extends Level2StateNew{
-		
-	private static final long serialVersionUID = 6056436154088977036L;
 
+	private static final long serialVersionUID = 6056436154088977036L;
+	private BufferedImage level3Background;
 	public Level3State(int level, MainFrame frame, GameStatus status, LevelLogic gameLogic, InputHandler inputHandler,
 			GraphicsManager graphicsMan, SoundManager soundMan) {
 		super(level, frame, status, gameLogic, inputHandler, graphicsMan, soundMan);
-	}	
-	
+		
+		try {
+
+			this.level3Background = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/level4.jpg"));
+
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "The graphic files are either corrupt or missing.",
+					"VoidSpace - Fatal Error", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	public void updateScreen(){
+		Graphics2D g2d = getGraphics2D();
+		GameStatus status = this.getGameStatus();
+
+		// save original font - for later use
+		if(this.originalFont == null){
+			this.originalFont = g2d.getFont();
+			this.bigFont = originalFont;
+		}
+
+		clearScreen();
+		g2d.drawImage(level3Background,null,0,0);
+		drawStars(50);
+		drawFloor();
+		drawPlatforms();
+		drawMegaMan();
+		drawAsteroid();
+		drawBullets();
+		drawBigBullets();
+		checkBullletAsteroidCollisions();
+		checkBigBulletAsteroidCollisions();
+		checkMegaManAsteroidCollisions();
+		checkAsteroidFloorCollisions();
+
+		// update asteroids destroyed (score) label  
+		getMainFrame().getDestroyedValueLabel().setText(Long.toString(status.getAsteroidsDestroyed()));
+		// update lives left label
+		getMainFrame().getLivesValueLabel().setText(Integer.toString(status.getLivesLeft()));
+		//update level label
+		getMainFrame().getLevelValueLabel().setText(Long.toString(status.getLevel()));
+
+
+	}
+
+
+
 	int position = rand.nextInt();
-	
+
 	@Override
 	protected void drawAsteroid() {
 		Graphics2D g2d = getGraphics2D();
-		
+
 		switch(position % 2) {
 		case 0:
 			if((asteroid.getX() + asteroid.getPixelsWide() >  0)) {
-				
+
 				//CHANGE THIS FOR ANGLE OF DESCENT, make this random
 				asteroid.translate(-asteroid.getSpeed(), asteroid.getSpeed());
-				
+
 				getGraphicsManager().drawAsteroid(asteroid, g2d, this);	
 			}
 			else {
 				//change delay thing for multiple asteroids
 				long currentTime = System.currentTimeMillis();
 				if((currentTime - lastAsteroidTime) > NEW_ASTEROID_DELAY){
-					
+
 					//Set Asteroid to the right of the screen, random Y position. make x random
 					asteroid.setLocation(this.getWidth() - asteroid.getPixelsWide(),
 							rand.nextInt(this.getHeight() - asteroid.getPixelsTall() - 32));
@@ -49,17 +100,17 @@ public class Level3State extends Level2StateNew{
 			break;
 		case 1:
 			if((asteroid.getX() + asteroid.getPixelsWide() < this.getWidth())) {
-				
+
 				//CHANGE THIS FOR ANGLE OF DESCENT, make this random
 				asteroid.translate(asteroid.getSpeed(), asteroid.getSpeed());
-				
+
 				getGraphicsManager().drawAsteroid(asteroid, g2d, this);	
 			}
 			else {
 				//change delay thing for multiple asteroids
 				long currentTime = System.currentTimeMillis();
 				if((currentTime - lastAsteroidTime) > NEW_ASTEROID_DELAY){
-					
+
 					//Set Asteroid to the right of the screen, random Y position. make x random
 					asteroid.setLocation(0 + asteroid.getPixelsWide(),
 							rand.nextInt(this.getHeight() - asteroid.getPixelsTall() - 32));
@@ -71,11 +122,11 @@ public class Level3State extends Level2StateNew{
 			}
 			break;
 		}
-		
-		
+
+
 	}
 
-	
+
 	//Plataformas
 	@Override
 	public Platform[] newPlatforms(int n){
